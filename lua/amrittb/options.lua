f65897e852f40128db656144d019badbe90e2960
Amrit Twanabasu <amrit.cas@gmail.com>
2023-12-14 10:25:54 +0900

    Add more remaps

--- lua/amrittb/options.lua
+++ lua/amrittb/options.lua
@@ -1,36 +1,52 @@
 -------------------------------
 -- Vim Options 
 -------------------------------
+-- Enable Fat cursor
+vim.opt.guicursor = ""
+
 -- Show line numbers
 vim.opt.number = true
+-- Enable relative line number
+vim.opt.relativenumber = true
+
 -- Enable mouse selection for all modes
 vim.opt.mouse = "a"
+
 -- Ignore case when searching text
 vim.opt.ignorecase = true
 -- Smart casing
 vim.opt.smartcase = true
+
 -- Remove highlight from last search
 vim.opt.hlsearch = false
+-- Incremental search
+vim.opt.incsearch = true
+
 -- Word wrap
 vim.opt.wrap = true
 -- Preserve indentation of wrapped lines
 vim.opt.breakindent = true
+
 -- Change tab size to 2
 vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
 -- Shift by same value as tabstops when >> or <<
 vim.opt.shiftwidth = 2
 -- Change tabs to spaces
 vim.opt.expandtab = true
+
+-- Always have 8 lines when scrolling
+vim.opt.scrolloff = 8
 -- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
+vim.opt.signcolumn = 'yes'
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
--- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
--- Enable relative line number
-vim.opt.relativenumber = true
+
+-- Fast updatetime
+vim.opt.updatetime = 50
+
+-- Add column
+vim.opt.colorcolumn = "100"
 
 -- Neovim File explorer
 vim.g.netrw_banner = 0
--- lua/amrittb/remap.lua
+++ lua/amrittb/remap.lua
@@ -1,20 +1,38 @@
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 -- Set leaderkey as <space>
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- See `:help vim.keymap.set()`
 -- No operation on normal and visual mode
 -- TODO: Terminal mode
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 -- Open Netrw
-vim.keymap.set('n', '<leader>e', vim.cmd.Ex)
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { noremap = true })
+-- TODO Fix conflict
+-- vim.keymap.set("v", "K", ":m ">-2<CR>gv=gv", { noremap = true })
 
 -- Helper remaps to move vertically
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true })
+vim.keymap.set("n", "n", "nzzzv", { noremap = true })
+vim.keymap.set("n", "N", "Nzzzv", { noremap = true })
 
 -- Split screen
 vim.keymap.set("n", "<leader>|", ":vsplit<CR><C-w>l", { noremap = true })
 vim.keymap.set("n", "<leader>-", ":split<CR><C-w>l", { noremap = true })
 
+-- Don"t loose paste register
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- Paste from OS copy register
+vim.keymap.set("n", "<leader>p", "\"+P")
+-- Copy to OS copy register
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- No operation for "Q"
+vim.keymap.set("n", "Q", "<nop>")
