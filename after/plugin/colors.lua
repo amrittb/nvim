9783b0417ff56280566509f945511cf5ed12913c
Amrit Twanabasu <amrit.cas@gmail.com>
2023-12-26 10:20:12 +0900

    Update colors

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -9,5 +9,46 @@ function SetColorScheme(color)
   vim.cmd.colorscheme(color)
 end
 
-SetColorScheme()
+require("tokyonight").setup({
+  styles = {
+    sidebars = "normal",
+    floats = "dark",
+  },
+  sidebars = {
+    "qf", "help", "terminal"
+  },
+  on_highlights = function(hl, c)
+    local prompt = "#2d3149"
+    hl.TelescopeNormal = {
+      bg = c.bg_dark,
+      fg = c.fg_dark,
+    }
+    hl.TelescopeBorder = {
+      bg = c.bg_dark,
+      fg = c.bg_dark,
+    }
+    hl.TelescopePromptNormal = {
+      bg = prompt,
+    }
+    hl.TelescopePromptBorder = {
+      bg = prompt,
+      fg = prompt,
+    }
+    hl.TelescopePromptTitle = {
+      bg = prompt,
+      fg = prompt,
+    }
+    hl.TelescopePreviewTitle = {
+      bg = c.bg_dark,
+      fg = c.bg_dark,
+    }
+    hl.TelescopeResultsTitle = {
+      bg = c.bg_dark,
+      fg = c.bg_dark,
+    }
+  end,
+})
+
+-- Enable tokyonight
+SetColorScheme("tokyonight-storm")
 
